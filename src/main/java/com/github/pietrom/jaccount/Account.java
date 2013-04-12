64634610838fb21e621726f6425964fb595422a0
Pietro Martinelli <amicofragile@gmail.com>
2013-04-12 11:59:04 +0200

    REFACTORING Real username management, based on an instance attribute

--- src/main/java/com/github/pietrom/jaccount/Account.java
+++ src/main/java/com/github/pietrom/jaccount/Account.java
@@ -1,11 +1,13 @@
 package com.github.pietrom.jaccount;
 
 public class Account {
-
+	private final String username;
+	
 	public Account(String username) {
+		this.username = username;
 	}
 
 	public String getUsername() {
-		return "pietrom";
+		return username;
 	}
 }
