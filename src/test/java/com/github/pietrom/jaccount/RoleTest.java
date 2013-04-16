dcfff16e943fd1f00e32be53f36797ea0a37c35e
Pietro Martinelli <amicofragile@gmail.com>
2013-04-16 22:46:10 +0200

    REFACTORING Extracted constant ROLE_NAME

--- src/test/java/com/github/pietrom/jaccount/RoleTest.java
+++ src/test/java/com/github/pietrom/jaccount/RoleTest.java
@@ -1,23 +1,25 @@
 package com.github.pietrom.jaccount;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
 public class RoleTest {
+	private static final String ROLE_NAME = "role-name";
 	private static final String KEY_A = "key-a";
 	private static final String VALUE = "value-a";
 
 	@Test
 	public void givenARoleWithoutPropertiesThenGetPropertyReturnsNull() throws Exception {
-		PropertiesSource role = new Role("role-name");
+		PropertiesSource role = new Role(ROLE_NAME);
 		assertNull(role.getProperty(KEY_A));
 	}
 	
 	@Test
 	public void givenARoleWithAPropertyThenItsGettable() throws Exception {
-		PropertiesSource account = new Role("role-name");
+		PropertiesSource account = new Role(ROLE_NAME);
 		account.setProperty(KEY_A, VALUE);
 		assertEquals(VALUE, account.getProperty(KEY_A));
 	}
