c64e80389ceafdcc53284fdd938c89daac207cba
Pietro Martinelli <amicofragile@gmail.com>
2013-04-17 22:27:50 +0200

    REFACTORING Extracted method which initializes test methods' common fixtures

--- src/test/java/com/github/pietrom/jaccount/RolesGroupTest.java
+++ src/test/java/com/github/pietrom/jaccount/RolesGroupTest.java
@@ -1,30 +1,35 @@
 package com.github.pietrom.jaccount;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RolesGroupTest {
+	private Role role1, role2;
+	private RolesGroup group;
+	
+	@Before
+	public void initFixtures() {
+		role1 = new Role("role-1");
+		role2 = new Role("role-2");
+		group = new RolesGroup("group");
+		group.addRole(role1);
+		group.addRole(role2);
+	}
 	@Test
 	public void aRolesGroupLooksOutLikeARole() throws Exception {
-		Role role1 = new Role("role-1");
-		Role role2 = new Role("role-2");
-		Role group = new RolesGroup("group");
-		((RolesGroup)group).addRole(role1);
-		((RolesGroup)group).addRole(role2);
-		assertTrue(group.matches("role-1"));
-		assertTrue(group.matches("role-2"));
-		assertFalse(group.matches("role-3"));
+		Role role = group;
+		assertTrue(role.matches("role-1"));
+		assertTrue(role.matches("role-2"));
+		assertFalse(role.matches("role-3"));
 	}
 	
 	@Test
 	public void rolesGroupDelegatesGetPropertyToNestedRoles() throws Exception {
-		Role role1 = new Role("role-1");
 		role1.setProperty("a-key", "a-value");
-		Role role2 = new Role("role-2");
-		RolesGroup group = new RolesGroup("group");
-		group.addRole(role1);
-		group.addRole(role2);
 		assertEquals("a-value", group.getProperty("a-key"));
 	}
 }
