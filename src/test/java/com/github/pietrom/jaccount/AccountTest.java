3936d8c81385ace5a5573f055ca1dde72920ad10
Pietro Martinelli <amicofragile@gmail.com>
2013-04-18 09:06:21 +0200

    TEST Account.hasRole given RoleGroup

--- src/test/java/com/github/pietrom/jaccount/AccountTest.java
+++ src/test/java/com/github/pietrom/jaccount/AccountTest.java
@@ -12,8 +12,9 @@ public class AccountTest {
 	private static final String KEY_A = "a-key";
 	private static final String VALUE = "a-value";
 	private static final String ROLE_A_NAME = "role-a";
+	private static final String ROLE_B_NAME = "role-b";
 	private static final Role ROLE_A = new Role(ROLE_A_NAME);
-	private static final Role ROLE_B = new Role("role-b");
+	private static final Role ROLE_B = new Role(ROLE_B_NAME);
 
 	@Test
 	public void usernameIsAccessible() throws Exception {
@@ -50,5 +51,18 @@ public class AccountTest {
 		roles.add(ROLE_B);
 		Account account = new Account(USERNAME, roles);
 		assertTrue(account.hasRole(ROLE_A_NAME));
+	}
+	
+	@Test
+	public void hasRoleGivenRoleGroup() throws Exception {
+		Collection<Role> roles = new HashSet<Role>();
+		RolesGroup group = new RolesGroup("a-group");
+		group.addRole(ROLE_A);
+		group.addRole(new Role("role-c"));
+		roles.add(group);
+		roles.add(ROLE_B);
+		Account account = new Account(USERNAME, roles);
+		assertTrue(account.hasRole(ROLE_A_NAME));
+		assertTrue(account.hasRole(ROLE_B_NAME));
 	}
 }
