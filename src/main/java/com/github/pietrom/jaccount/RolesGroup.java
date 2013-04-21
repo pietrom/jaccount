81f7aa8837243279717a96238e5be31789ba8c5d
Pietro Martinelli <amicofragile@gmail.com>
2013-04-21 16:14:23 +0200

    REFACTORING Single exit point for RolesGroup.matches() method

--- src/main/java/com/github/pietrom/jaccount/RolesGroup.java
+++ src/main/java/com/github/pietrom/jaccount/RolesGroup.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 
 public class RolesGroup extends Role {
 	private final Collection<Role> roles;
+
 	public RolesGroup(String name) {
 		super(name);
 		this.roles = new LinkedList<Role>();
@@ -14,30 +15,29 @@ public class RolesGroup extends Role {
 	public void addRole(Role role) {
 		roles.add(role);
 	}
-	
+
 	@Override
 	public boolean matches(String roleName) {
-		for (Role role : roles) {
-			if(role.matches(roleName)) {
-				return true;
-			}
+		boolean matched = false;
+		Iterator<Role> it = roles.iterator();
+		while(!matched && it.hasNext()) {
+			matched = it.next().matches(roleName);
 		}
-		return false;
+		return matched;
 	}
-	
+
 	@Override
 	public Object getProperty(String key) {
 		Object value = super.getProperty(key);
-		if(value != null) {
-			return value;
-		}
-		Iterator<Role> it = roles.iterator();
-		while(value == null && it.hasNext()) {
-			value = it.next().getProperty(key);
+		if (value == null) {
+			Iterator<Role> it = roles.iterator();
+			while (value == null && it.hasNext()) {
+				value = it.next().getProperty(key);
+			}
 		}
 		return value;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder(super.toString()).append(": [");
